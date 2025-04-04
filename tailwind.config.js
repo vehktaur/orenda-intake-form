29bbcf2e0da86b762818c3eee95a717207bfe69b
vehktaur <victorakhihiero@gmail.com>
2025-04-04 10:38:48 +0100

    fix: update font paths and background image URLs for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,43 +2,34 @@ import fluid, { extract, screens, fontSize } from 'fluid-tailwind';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ['class'],
-    content: { files: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], extract },
+  darkMode: ['class'],
+  content: { files: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], extract },
 
   theme: {
-	fontSize,
-	screens,
-  	extend: {
-  		backgroundImage: {
-  			dotted: 'url(/src/assets/dotted.png)'
-  		},
-  		colors: {
-  			'orenda-purple': '#2E0086',
-  			'orenda-green': '#127801'
-  		},
-  		fontFamily: {
-  			'open-sans': [
-  				'Open Sans"',
-  				'sans-serif'
-  			],
-  			heading: [
-  				'Ogg Text TRIAL',
-  				
-  			],
-  			'dm-sans': [
-  				'DM Sans"',
-  				'sans-serif'
-  			]
-  		},
-  		screens: {
-  			xs: '20rem'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    fontSize,
+    screens,
+    extend: {
+      backgroundImage: {
+        dotted: 'url(/src/assets/dotted.png)',
+      },
+      colors: {
+        'orenda-purple': '#2E0086',
+        'orenda-green': '#127801',
+      },
+      fontFamily: {
+        'open-sans': ['Open Sans"', 'sans-serif'],
+        heading: ['Ogg Text TRIAL', 'serif'],
+        'dm-sans': ['DM Sans"', 'sans-serif'],
+      },
+      screens: {
+        xs: '20rem',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
   },
   plugins: [
     fluid,
@@ -46,6 +37,6 @@ export default {
       nocompatible: true,
       preferredStrategy: 'pseudoelements',
     }),
-      require("tailwindcss-animate")
-],
+    require('tailwindcss-animate'),
+  ],
 };
